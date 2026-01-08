346e917196cff06da4b824ddd5f13268ea512703
asif256000 <asif256000@gmail.com>
2026-01-08 16:36:59 -0500

    fix: clean up whitespace

--- ghostparser/parser.py
+++ ghostparser/parser.py
@@ -1,9 +1,9 @@
 def generate_response(user_input):
     """Generate a response based on user input.
-    
+
     Args:
         user_input: The input string from the user.
-        
+
     Returns:
         A response string based on the input.
     """
